0977d2da0e65867d1c704a71e6d8bb4c6a5b707e
blockiosaurus <blockiosaurus@gmail.com>
2023-12-17 14:59:48 -0500

    Fixing PDA seed for brawl.

--- Unity/NFTBrawl/Assets/Game/Scripts/Solana/BrawlAnchorService.cs
+++ Unity/NFTBrawl/Assets/Game/Scripts/Solana/BrawlAnchorService.cs
@@ -620,7 +620,7 @@ public class BrawlAnchorService : MonoBehaviour
         };
 
         PublicKey.TryFindProgramAddress(new[]
-               {Encoding.UTF8.GetBytes("brawler"), ColosseumPDA.KeyBytes, BitConverter.GetBytes(CurrentColosseum.NumBrawls)},
+               {Encoding.UTF8.GetBytes("brawl"), ColosseumPDA.KeyBytes, BitConverter.GetBytes(CurrentColosseum.NumBrawls)},
            AnchorProgramIdPubKey, out BrawlPDA, out byte brawlBump);
 
         StartBrawlAccounts startBrawlAccounts = new StartBrawlAccounts
